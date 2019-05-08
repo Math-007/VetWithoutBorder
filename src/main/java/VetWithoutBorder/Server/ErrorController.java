0c3f80563ceb36f17f29e4dad8285e948e0a5d2d
Mathieu Marchand <mathieu.marchand@polymtl.ca>
2019-05-08 14:08:48 -0400

    add sql error handler

--- src/main/java/VetWithoutBorder/Server/ErrorController.java
+++ src/main/java/VetWithoutBorder/Server/ErrorController.java
@@ -5,11 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.PersistenceException;
+
 @ControllerAdvice
 public class ErrorController {
 
     @ExceptionHandler(value = EntityNotFoundError.class)
     public ResponseEntity<QueryError> entityNotFound(EntityNotFoundError exception) {
         return new ResponseEntity<>(exception.errorMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = PersistenceException.class)
+    public ResponseEntity<QueryError> sqlError(PersistenceException exception) {
+        return new ResponseEntity<>(new QueryError(exception.getCause().getCause().getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
